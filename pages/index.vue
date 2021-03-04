a85825d6717f7969efc707e1b65213ace1ab5154
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2021-03-05 07:33:02 +0900

    Add nuxt link for index

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,9 @@
   <div class="flex flex-row space-x-4">
     <div class="w-60 p-4">
       <div class="font-bold text-xl">
-        PREFETCH TODOS
+        <nuxt-link to="/" @mouseenter.native="onMouseEnter"
+          >PREFETCH TODOS
+        </nuxt-link>
       </div>
     </div>
     <div class="p-4">
@@ -12,13 +14,24 @@
 </template>
 
 <script>
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, ref, useFetch } from "@nuxtjs/composition-api";
+import usePrefetchStore from "~/composable/prefetch-store";
 
 export default defineComponent({
   setup() {
+    const { fetch } = useFetch(async () => {
+      await usePrefetchStore("todos", "/api/todos", {
+        resetCache: true,
+      });
+    });
 
-  }
-})
+    const onMouseEnter = () => {
+      fetch();
+    };
+
+    return { onMouseEnter };
+  },
+});
 </script>
 
 <style>
